19121d2c75df8c374688a27dd616d22636dc6af2
XeindsTwo <yourihelo1971@gmail.com>
2026-03-11 16:39:43 +0300

    hotfix

--- src/CheckPassword.py
+++ src/CheckPassword.py
@@ -2,14 +2,16 @@ class CheckPassword:
     def __init__(self, password_user: str):
         self.password_user = password_user
 
-    def validatePassword(self):
+    @staticmethod
+    def validatePassword(password: str):
         hasUpper = False
         hasDigit = False
 
-        for char in self.password_user:
-            if len(self.password_user) < 8:
-                raise ValueError("Длина строки меньше 8 символов")
-            elif char.isalpha() and char.isupper():
+        if len(password) < 8:
+            raise ValueError("Длина строки меньше 8 символов")
+
+        for char in password:
+            if char.isalpha() and char.isupper():
                 hasUpper = True
             elif char.isdigit():
                 hasDigit = True
